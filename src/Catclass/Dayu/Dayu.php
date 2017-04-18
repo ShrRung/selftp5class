1d613100e4385e06d7c69a3428d2ea60b2b07ee1
ShrRung <1026825079@qq.com>
2017-04-18 23:56:19 +0800

    init-0.2

--- src/Catclass/Dayu/Dayu.php
+++ src/Catclass/Dayu/Dayu.php
@@ -9,17 +9,11 @@
 // |                                                                      |
 // +----------------------------------------------------------------------+
 
-namespace think\geetest;
-use think\Config;
-use think\Session;
-use think\geetest\GeetestLib;
-class GeetestlibController
+namespace Catclass\Dayu;
+class Dayu
 {
-    public function index()
+    public function info()
     {
-        $geetest = new GeetestLib((array)Config::get('geetest'));
-        Session::set('gt_user_id',$_SERVER['REQUEST_TIME']);
-        Session::set('gt_server_status',$geetest->pre_process(Session::get('gt_user_id')));
-        return  $geetest->get_response_str();
+        echo 'Dayu';
     }
 }
